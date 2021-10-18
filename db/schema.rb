10e58533423f2bad9cea404ab83e4f87503a0616
Michal Nowak <minow1980@gmail.com>
2021-10-18 23:22:05 +0200

    Subscription scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_18_192119) do
+ActiveRecord::Schema.define(version: 2021_10_18_211019) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 2021_10_18_192119) do
     t.decimal "price", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "payment_token", null: false
+    t.integer "customer_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["product_id"], name: "index_subscriptions_on_product_id"
   end
 
 end
